c0fe815c3d61947d4c35dec397b40f95e4d4c68e
Thorben Below <56894536+thorbenbelow@users.noreply.github.com>
2024-02-19 21:34:02 +0100

    include dependency install in bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,26 @@
 #!/usr/bin/env bash
 
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+	[ "$UID" -eq 0 ] || exec sudo bash "$0" "$@"
+	apt update -y && apt install -y git stow curl zsh vim
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+	brew install git stow curl zsh nvim
+fi
+
 git clone https://github.com/thorbenbelow/dotfiles-stow.git $HOME/.dotfiles
-cd $HOME/dotfiles
+cd $HOME/.dotfiles
+
 
 if command -v stow &> /dev/null
 then
-	find . -type d -maxdepth 1 -mindepth 1 -exec bash -c ' stow $(basename "$0")' {} \;
+#	find . -type d -maxdepth 1 -mindepth 1 -exec bash -c ' stow $(basename "$0")' {} \;
+	stow -R -t $HOME git
+
+	mkdir -p $HOME/.config/nvim
+	stow -R -t $HOME/.config/nvim nvim
+
+	stow -R -t $HOME zsh
+
 fi
 
 if command -v zsh &> /dev/null
@@ -14,10 +29,4 @@ then
 	git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 	chsh -s $(which zsh)
-fi
-
-if command -v nvim &> /dev/null
-then
-	sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-	vim 'PlugInstall --sync' +qa
 fi
